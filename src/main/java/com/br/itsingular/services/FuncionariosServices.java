e8740c61932b20c967311dd034d49168bbc32fbc
crodrigues <99739719+crodrigues-itsingular@users.noreply.github.com>
2022-03-16 15:05:28 -0300

    Criação de Validação da Data de Contratação - EM ANDAMENTO
    Criação de Validação da Data de Contratação - EM ANDAMENTO

--- src/main/java/com/br/itsingular/services/FuncionariosServices.java
+++ src/main/java/com/br/itsingular/services/FuncionariosServices.java
@@ -1,5 +1,8 @@
 package com.br.itsingular.services;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,5 +38,29 @@ public class FuncionariosServices {
 		Query q= new Query();
 		q.addCriteria(Criteria.where("_id").is(entity.getId()));
 		template.findAndModify(q, UpdateEmployeesConvert.updateDadosfuncionarios(entity) , Funcionarios.class);
+	}
+	public void validaDataContratacao(LocalDate dataInicio, LocalDate dataFim) {
+		LocalDate dataAtual = LocalDate.now();
+		Date date = Date.valueOf(dataAtual);
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataAtualParse = format.format(date);
+		String[] anoAtualArray = dataAtualParse.split("/");
+		String anoAtual = anoAtualArray[2];
+		
+		Date dateInicio = Date.valueOf(dataInicio);
+		SimpleDateFormat formatInicio = new SimpleDateFormat("dd/MM/yyyy");
+		String dataInicioParse = format.format(dateInicio);
+		String[] dataInicioArray = dataInicioParse.split("/");
+		String anoInicio = dataInicioArray[2];
+		
+		Date dateFim = Date.valueOf(dataFim);
+		SimpleDateFormat formatFim = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFimParse =  format.format(dateFim);
+		String[] dataFimArray = dataFimParse.split("/");
+		String anoFim = dataFimArray[2];	
+		
+		if (!anoAtual.equals(anoInicio) && anoAtual.equals(anoFim)) {
+			
+		}
 	}
 }
