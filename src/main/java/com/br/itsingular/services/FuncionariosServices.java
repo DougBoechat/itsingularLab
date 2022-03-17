1ac7815369bd2b128bf786e8d89cddcdfe14446e
isayasmine <irosa@itsingular.com.br>
2022-03-17 13:07:54 -0300

    Validação data de contratação (em andamento).
    Validação data de contratação parte do backend ponta.
    Falta criar um alert no front.

--- src/main/java/com/br/itsingular/services/FuncionariosServices.java
+++ src/main/java/com/br/itsingular/services/FuncionariosServices.java
@@ -6,61 +6,66 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import com.br.itsingular.controller.CadastrarFuncionarios;
 import com.br.itsingular.entity.Funcionarios;
 import com.br.itsingular.repository.FuncionariosRepository;
 import com.br.itsingular.utils.UpdateEmployeesConvert;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class FuncionariosServices {
-	
+
 	@Autowired
-	private FuncionariosRepository funcionariosRepository; 
-	
+	private FuncionariosRepository funcionariosRepository;
+
 	@Autowired
 	private MongoTemplate template;
-	
+
 	public List<Funcionarios> findFuncionarios() {
 		return funcionariosRepository.findAll();
 	}
+
 	public Optional<Funcionarios> findFuncionariosById(String id) {
 		return Optional.ofNullable(funcionariosRepository.findById(id)).get();
 	}
+
 	public void insert(Funcionarios entity) {
 		funcionariosRepository.insert(entity);
 	}
+
 	public void update(Funcionarios entity) {
-		Query q= new Query();
+		Query q = new Query();
 		q.addCriteria(Criteria.where("_id").is(entity.getId()));
-		template.findAndModify(q, UpdateEmployeesConvert.updateDadosfuncionarios(entity) , Funcionarios.class);
+		template.findAndModify(q, UpdateEmployeesConvert.updateDadosfuncionarios(entity), Funcionarios.class);
 	}
-	public void validaDataContratacao(LocalDate dataInicio, LocalDate dataFim) {
-		LocalDate dataAtual = LocalDate.now();
-		Date date = Date.valueOf(dataAtual);
-		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-		String dataAtualParse = format.format(date);
-		String[] anoAtualArray = dataAtualParse.split("/");
-		String anoAtual = anoAtualArray[2];
+
+	public Boolean validaDataContratacao(LocalDate dataInicio, LocalDate dataFim) {
+
+		Integer anoAtual = LocalDate.now().getYear();
+		Integer anoInicio = dataInicio != null ? dataInicio.getYear() : null;
+		Integer anoFinal = dataFim != null ? dataFim.getYear() : null;
 		
-		Date dateInicio = Date.valueOf(dataInicio);
-		SimpleDateFormat formatInicio = new SimpleDateFormat("dd/MM/yyyy");
-		String dataInicioParse = format.format(dateInicio);
-		String[] dataInicioArray = dataInicioParse.split("/");
-		String anoInicio = dataInicioArray[2];
-		
-		Date dateFim = Date.valueOf(dataFim);
-		SimpleDateFormat formatFim = new SimpleDateFormat("dd/MM/yyyy");
-		String dataFimParse =  format.format(dateFim);
-		String[] dataFimArray = dataFimParse.split("/");
-		String anoFim = dataFimArray[2];	
-		
-		if (!anoAtual.equals(anoInicio) && anoAtual.equals(anoFim)) {
+		if (anoInicio != null && anoFinal != null) {
+			if (anoInicio >= anoAtual && anoFinal >= anoAtual) {
+				return Boolean.TRUE;
+			}
 			
+			if (anoInicio < anoAtual && anoFinal < anoAtual) {
+				throw new NumberFormatException("Ano inválido");
+			}
 		}
+
+		return Boolean.FALSE;
 	}
+
 }
